9d9736c61d91fa090b3839851e69d01439ad40f6
hamidie00235 <ahmathisseinh@gmail.com>
2023-05-06 17:29:31 +0300

    sepet filtreme yerine ürün filtreme yapildi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import { Container,Input, List,SimpleGrid,ThemeIcon} from '@mantine/core';
+import { IconCircleCheck, IconCircleDashed } from '@tabler/icons-react';
 import './App.css';
-
+import Card from "./components/Card";
+const storeItems = [{
+  name:"Apple",
+  src:"Apple-Tools",
+  price:23,},
+  
+    {
+      name:"Camera",
+      src:"Camera",
+      price:4,},
+      { 
+      name:"Apple",
+      src:"Apple-Tools2",
+      price:2},
+      { 
+      name:"Watch",
+      src:"Watch",
+      price:6,},
+      { 
+      name:"Kit",
+      src:"Kits",
+      price:8,}
+    ,
+      {
+      name:"Tool",
+      src:"Tools",
+      price:5,}
+      ,
+      {
+      name:"CE",
+      src:"Ce",
+      price:5,}]
 function App() {
+  let [basketItems,setbasketItems]=useState([])
+  let [searchValue,setSearchValue] = useState("")
+  let filteredItems =storeItems.filter((item) =>item.name.indexOf(searchValue) >= 0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+        <Input.Wrapper label="Arama">
+  <Input onChange={(e) => setSearchValue(e.target.value)} />
+</Input.Wrapper>
+   <SimpleGrid cols={3} className="store">
+
+  
+      {filteredItems.map(({name,src}) => {
+        return <Card 
+        key={name}
+         name={name} 
+         src={src}
+         onAdd ={() => setbasketItems([...basketItems,{name}])} />
+
+        })  }
+      </SimpleGrid>
+  
+      <List className="List"
+      spacing="xs"
+      size="sm"
+      center
+      icon={
+        <ThemeIcon color="teal" size={24} radius="xl">
+          <IconCircleCheck size="1rem" />
+        </ThemeIcon>
+      }
+    >
+        {basketItems.map(({name}, index) => ( 
+           <List.Item key={index}>{name}</List.Item>
+           ))}
+    
+    </List>
+      </Container>
+      
   );
 }
 
